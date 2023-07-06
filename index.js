8229a7a4a215a612ebbeee68b4fa4a909a5c5467
Sharon2408 <sharonantoy2408@gmail.com>
2023-07-06 14:25:21 +0530

    cg

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router("acecraft.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; // Chose port from here like 8080, 3001 etc
 
